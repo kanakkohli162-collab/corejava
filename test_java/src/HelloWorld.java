0a5fe670df4bb3c671d9ac769b96f98309a2d9aa
kanakkohli162-collab <kanakkohli162@gmail.com>
2026-01-13 18:34:39 +0530

    ok

---
+++ test_java/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        int result = a * b;
+
+        System.out.println("Multiplication = " + result);
+    }
+}
